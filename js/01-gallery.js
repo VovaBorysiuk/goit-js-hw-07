6ffd6d66ad6bd609049e0d60790ba84b6ab6671a
Vova <v.borysiuk91@gmail.com>
2023-09-05 15:39:01 +0300

    lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createGalleryItem = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+    <a class = "gallery__link" href="${original}" >
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" >
+    </a>
+  </li>`
+  )
+  .join("");
+
+galleryList.innerHTML = createGalleryItem;
+
+galleryList.addEventListener("click", onImageClick);
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const largeImageUrl = event.target.dataset.source
+  const instance = basicLightbox.create(
+    `<img src="${largeImageUrl}" width="800" height="600" >`);
+  instance.show();
+
+  galleryList.addEventListener("keydown", (event) => {
+    
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  })
+}
+
